fb122796c3796ec766489c9250412f2e2baf5122
sakthivelk22 <sakthivel.k22@gmail.com>
2015-09-05 07:00:51 +0530

    Test Commits
    Test Commit

--- Logger/main.cpp
+++ Logger/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
 	_logger::Logger* logInstance = _logger::Logger::getInstance("test.log");
 	logInstance->log(_logger::SEVERE,"Test 123");
 	logInstance->log(_logger::FINE,"Help");
+	logInstance->log(_logger::FINE,"Another Help");
 	
 	getchar();
 	return 0;
